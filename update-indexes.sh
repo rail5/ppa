2d62e76fb4187fdefdac2df0d1ee96bcbf2679b9
rail5 <andrew@rail5.org>
2023-03-31 05:17:59 -0400

    Improved directory indexing

--- update-indexes.sh
+++ update-indexes.sh
@@ -3,10 +3,10 @@
 function create_gnu_index ()
 {
     # call it right or die
-    [[ $# != 4 ]] && echo "bad args. do: $FUNCNAME '/DOCUMENT_ROOT/mainsubdir' '/files/things/' 'gnu.askapache.com' '/mainsubdir/files/things'" && exit 2
+    [[ $# != 5 ]] && echo "bad args. do: $FUNCNAME '/DOCUMENT_ROOT/mainsubdir' '/files/things/' 'gnu.askapache.com' '/mainsubdir/files/things' '/mainsubdir/files'" && exit 2
   
     # D is the doc_root containing the site
-    local L= D="$1" SUBDIR="$2" DOMAIN="$3" URLPATH="$4" F=
+    local L= D="$1" SUBDIR="$2" DOMAIN="$3" URLPATH="$4" PARENTDIR="$5" F=
 
     # The index.html file to create
     F="${D}index.html"
@@ -30,6 +30,7 @@ function create_gnu_index ()
         echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">';
         echo "<html><head><title>Index of http://${DOMAIN}${URLPATH}</title></head>";
         echo "<body><h1 id="Index_SUBDIR">Index of ${URLPATH} <a class="sl" href="#Index_SUBDIR"></a></h1><pre>      Name                                        Last modified      Size";
+        echo "      <a href=\"${PARENTDIR}\">Parent Directory</a>";
 
         # start of content output
         (
@@ -64,13 +65,13 @@ function create_gnu_index ()
 function recursively_create_gnu_index() {
 	STOREIFS="$IFS"
 
-	local startdir="$1" domain="$2" relpath=""
+	local startdir="$1" domain="$2" parentdirectory="$3" relpath=""
 	
 	relpath="${startdir/"$initdir"/""}"
 	
 	echo "Indexing $relpath"
 	
-	create_gnu_index "$startdir" "/" "$domain" "$relpath"
+	create_gnu_index "$startdir" "/" "$domain" "$relpath" "$parentdirectory"
 	
 	cd $startdir
 	
@@ -78,7 +79,7 @@ function recursively_create_gnu_index() {
 	
 	for i in $(ls); do
 		if [[ -d $startdir$i ]]; then
-			recursively_create_gnu_index "$startdir$i/" "$domain"
+			recursively_create_gnu_index "$startdir$i/" "$domain" "$startdir"
 			continue
 		fi
 	done
@@ -93,4 +94,4 @@ function recursively_create_gnu_index() {
 
 initdir=$(pwd)
 
-recursively_create_gnu_index "$initdir/debian/" "deb.rail5.org"
+recursively_create_gnu_index "$initdir/debian/" "deb.rail5.org" "/"
